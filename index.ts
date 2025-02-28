27a56c90a80c775aeccfd697bd71ef98e6c65f3c
Ardian <ardianmaliqaj49@gmail.com>
2025-02-28 14:03:54 +0100

    creating the basics for transpiler

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as fs from "fs"
+import * as ts from "typescript"
+import { Transpiler } from "./src/transpiler"
+
+// Read the source code from another TypeScript file
+const fileName = "C:\\Users\\Nora\\OneDrive\\Desktop\\flow\\src\\main.ts" // Path to your TypeScript file
+const sourceCode = fs.readFileSync(fileName, "utf-8")
+
+// Parse the TypeScript code into an AST
+const sourceFile = ts.createSourceFile(fileName, sourceCode, ts.ScriptTarget.Latest)
+
+const ast = JSON.parse(JSON.stringify(sourceFile))
+
+const transpiler = new Transpiler()
+
+transpiler.transpile(ast)
+
+console.log(transpiler.stack)
